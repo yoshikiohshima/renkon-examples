e9c667602939ada967aec71bf3a46b09d4ddbb04
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-04-24 12:16:29 -0700

    some tests

--- component2.js
+++ component2.js
@@ -1,9 +1,7 @@
-export function foo(multiplier, a, h) {
+export function foo({multiplier, a, h}) {
     const foo = Events.receiver();
     const m = Behaviors.collect(1, foo, (old) => old + 1);
     const c = multiplier * a + m;
-    const html = h("div", {color: "#d22", onclick: evt => {
-        Events.send(foo, true)
-    }}, c);
-    return [html];
+    const html = h("div", {style: {color: "#d22"}}, c);
+    return {html};
 }
