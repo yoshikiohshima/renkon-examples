310ec68d4844aa0b56a7d25a039e431d40fff24d
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-07-03 14:08:04 -0700

    update reference to the lib

--- component.js
+++ component.js
@@ -1,7 +1,7 @@
-export function foo(a, b) {
+export function foo({a, b}) {
     const c = a + 42;
     const d = b + Events.timer(100);
     const {h} = import("../preact.standalone.module.js");
     const html = h("div", {color: "#d22"}, c, d);
-    return [html];
+    return {html};
 }
--- component2.js
+++ component2.js
@@ -1,7 +1,10 @@
-export function aComponent({b}) {
-    const net = fetch(b);
-    const a = 42;
-    return {a, net};
+export function foo({multiplier, a, h}) {
+    const foo = Events.receiver();
+    const bar = Events.timer(multiplier * 500);
+    const m = Behaviors.collect(1, Events.or(foo, bar), (old) => old + 1);
+    const c = multiplier * a + m;
+    const html = h("div", {onclick: () => Events.send(foo, true), style: {color: "#d22"}}, c);
+    return {html};
 }
 
 /* globals Events Behaviors */
